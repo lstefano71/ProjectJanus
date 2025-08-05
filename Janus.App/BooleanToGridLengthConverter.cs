fc03e68594f6f204594da6a3a1efe899536b616c
stf <snlh@simcorp.com>
2025-08-05 19:40:55 +0200

    Add BooleanToGridLengthConverter for dynamic column widths
    Updated ResultsView.xaml to include a new converter,
    BooleanToGridLengthConverter, enabling dynamic width
    adjustments of grid columns based on the IsExpanded
    property of an Expander. Restructured layout with new
    ColumnDefinitions and organized elements into a grid
    format. Simplified DataGridTextColumn bindings by
    removing unnecessary Mode=OneWay specification.
    
    Introduced BooleanToGridLengthConverter class in
    BooleanToGridLengthConverter.cs to implement
    IValueConverter, facilitating the conversion of boolean
    values to GridLength for responsive UI design.

---
+++ Janus.App/BooleanToGridLengthConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Janus.App;
+
+public class BooleanToGridLengthConverter : IValueConverter
+{
+    public GridLength ExpandedWidth { get; set; } = new GridLength(220);
+    public GridLength CollapsedWidth { get; set; } = new GridLength(0);
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b && b)
+            return ExpandedWidth;
+        return CollapsedWidth;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
